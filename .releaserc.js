066c96bf15b6a72148cd7349a0a4c9f471ac871c
zlatan <zlatan.pham@gmail.com>
2020-12-17 20:31:40 +0700

    chore: update integrate semantic-release round 3

---
+++ .releaserc.js
@@ -0,0 +1,26 @@
+const hooks = require('semantic-release-monorepo-hooks');
+const output = hooks();
+const PACKAGE_NAME =
+  process.env.LERNA_PACKAGE_NAME || process.env.npm_package_name;
+
+module.exports = {
+  branch: 'master',
+  tagFormat: `${PACKAGE_NAME}@\${version}`,
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    {
+      path: '@semantic-release/git',
+      message:
+        'chore(' +
+        output.package +
+        '): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ],
+  publish: ['@semantic-release/npm'],
+  verifyConditions: ['@semantic-release/npm', '@semantic-release/git'],
+  monorepo: {
+    analyzeCommits: ['@semantic-release/commit-analyzer'],
+    generateNotes: ['@semantic-release/release-notes-generator'],
+  },
+};
