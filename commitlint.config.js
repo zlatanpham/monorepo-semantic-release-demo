1d858f65247766c9a8702068d9163f038629d98c
zlatan <zlatan.pham@gmail.com>
2020-12-17 23:15:49 +0700

    revert: releaserc

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,24 +5,5 @@ module.exports = {
     'scope-empty': [1, 'never'],
     'scope-case': [2, 'always', ['lower-case']],
     'header-max-length': [2, 'always', 72], // github commit without being trucated
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'hotfix',
-        'release',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'chore',
-        'revert',
-        'ci',
-        'pkg',
-      ],
-    ],
   },
 };
