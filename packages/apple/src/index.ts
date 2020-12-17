47b285121abbc44280d86bcd18297dc4207fb3b8
zlatan <zlatan.pham@gmail.com>
2020-12-17 16:18:07 +0700

    chore: init

---
+++ packages/apple/src/index.ts
@@ -0,0 +1,3 @@
+export function printMe(message: string) {
+  return message;
+}
---
+++ packages/orange/src/index.ts
@@ -0,0 +1,5 @@
+import { printMe } from '@tpham0/apple';
+
+export function callMe(message: string) {
+  console.log(printMe(message));
+}
