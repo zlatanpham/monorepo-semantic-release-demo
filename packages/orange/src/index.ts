002b1a05ed7174b2bcdb0d8d51c9d3d8467671ca
zlatan <zlatan.pham@gmail.com>
2020-12-17 18:18:43 +0700

    feat(orange): add sum() method
    affects: @tpham0/orange

--- packages/orange/src/index.ts
+++ packages/orange/src/index.ts
@@ -1,5 +1,9 @@
-import { printMe } from '@tpham0/apple';
+import { printMe, add } from '@tpham0/apple';
 
 export function callMe(message: string) {
   console.log(printMe(message));
+}
+
+export function sum(list: number[]) {
+  return list.reduce((a, c) => add(a, c), 0);
 }
