5f7bae5796382ee9a6dd6ab8240281e51402d0cb
zlatan <zlatan.pham@gmail.com>
2020-12-22 23:40:45 +0700

    chore: bump package

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['apple', 'orange']],
+    'scope-enum': [2, 'always', ['apple', 'orange', 'release']],
     'scope-empty': [1, 'never'],
     'scope-case': [2, 'always', ['lower-case']],
     'header-max-length': [2, 'always', 72], // github commit without being trucated
